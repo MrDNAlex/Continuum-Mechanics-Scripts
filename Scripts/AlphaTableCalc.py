b14b50d7863ca325f078486f41a12e0c248e922a
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-05-24 12:35:15 -0400

    Added Comments to the Scripts

--- Scripts/AlphaTableCalc.py
+++ Scripts/AlphaTableCalc.py
@@ -1,26 +1,25 @@
 import string
 
-
+# Calculates the Dot Product between 2 Vectors
 def DotProduct(Vector1, Vector2):
     DotProduct = 0
     for i in range(0, len(Vector1)):
         DotProduct += Vector1[i] * Vector2[i]
     return DotProduct
 
-
+# Calculates the Magnitude of a Vector
 def Magnitude(Vector):
     Magnitude = 0
     for i in range(0, len(Vector)):
         Magnitude += Vector[i] ** 2
     return Magnitude
 
-
+# Prompts a user to enter the values for a vector
 def AskForVector():
     x = float(input("Enter the x value of the vector: "))
     y = float(input("Enter the y value of the vector: "))
     z = float(input("Enter the z value of the vector: "))
     return x, y, z
-
 
 print("Enter the Values for the New reference Axis X Vector")
 VecXNew = AskForVector()
@@ -40,23 +39,28 @@ VecYOld = AskForVector()
 print("Enter the Values for the Old reference Axis Z Vector")
 VecZOld = AskForVector()
 
+# Stores the Array of New Vectors
 NewVectors = [VecXNew, VecYNew, VecZNew]
 
+# Stores the Array of Old Vectors
 OldVectors = [VecXOld, VecYOld, VecZOld]
 
-
+# Loops through each New Vector and Old Vector and Calculates the Angle between them
 for i in range(0, len(NewVectors)):
     for j in range(0, len(OldVectors)):
 
+        # Gets the Magnitude of the New and Old Vectors
         magNew = Magnitude(NewVectors[i])
         magOld = Magnitude(OldVectors[j])
 
+        # Calculates the Dot Product of the New and Old Vectors
         dotProduct = DotProduct(NewVectors[i], OldVectors[j])
 
+        # Calculates the Angle between the New and Old Vectors
         angle = dotProduct / (magNew * magOld)
 
-        strAngle: string = (
-            str(dotProduct) + "/" + "sqrt(" + str(magNew) + "*" + str(magOld) + ")"
-        )
+        # Formats the Angle into a String
+        strAngle: string = str(dotProduct) + "/" + "sqrt(" + str(magNew) + "*" + str(magOld) + ")"
 
+        # Prints the Angle between the New and Old Vectors
         print("a_" + str(i+1) + str(j+1) + " = "+strAngle)
--- Scripts/Homework.py
+++ Scripts/Homework.py
@@ -1,14 +1,18 @@
-
 # Homework 2, Tedious Sigma Sum Calculation
 
-
+# Prompts the user for the Index of the New Axis (X = 1, Y = 2, Z = 3)
 newAxisIndex = input("Enter the new axis index: ")
+
+# Prompts the user for the Index of the Old Axis (X = 1, Y = 2, Z = 3)
 oldAxisIndex = input("Enter the old axis index: ")
 
+# Prints A Statement to the User Saying that S = Sigma
 print("In this case S = Sigma")
 
+# Prints the Left side of the Equation
 print("S'_" + str(newAxisIndex) + str(oldAxisIndex) + " * A" + " = ")
 
+# Loops through each Sigma and Alpha and Prints the Additions for the Eqaution 
 for i in range(0, 3):
     for j in range(0, 3):
 
